aeca25ff324b5c96ce438e97b93320be5fb0ffc6
Alexander May <guitarx3@gmail.com>
2019-05-25 12:22:18 +0200

    fix tests, allow for submit via enter press

--- src/app/calculators/one-rep-max-calculator/one-rep-max-calculator.service.spec.ts
+++ src/app/calculators/one-rep-max-calculator/one-rep-max-calculator.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OneRepMaxCalculatorService } from './one-rep-max-calculator.service';
+import { RepMax } from 'src/app/models/rep-max';
 
 describe('OneRepMaxCalculatorService', () => {
     let service: OneRepMaxCalculatorService;
@@ -14,11 +15,10 @@ describe('OneRepMaxCalculatorService', () => {
     });
 
     it('should return the average result of all methods', () => {
-        const w = 100;
-        const r = 5;
+        const input: RepMax = { weight: 100, reps: 5 };
 
-        const calculatedMax = service.calculateOneRepMax(w, r);
-        const e = (service as any).epley(w, r);
+        const calculatedMax = service.calculateOneRepMax(input);
+        const e = (service as any).epley(input.weight, input.reps);
 
         expect(calculatedMax).toBe(Math.round(e / 1));
     });
@@ -26,17 +26,23 @@ describe('OneRepMaxCalculatorService', () => {
     it('should correctly calculate 1-10 rep maxes', () => {
         const expectedResults = [100, 95, 92, 89, 86, 83, 81, 79, 77, 73];
 
-        const actualResult = service.calculateNRepMax(100);
+        const actualResult = service.calculateNRepMax({ weight: 100, reps: 1 });
         for (let i = 0; i < expectedResults.length; i++) {
             expect(actualResult[i].weight).toBe(expectedResults[i]);
         }
     });
 
     it('should return correct numbers for one repetition', () => {
-        const input = 100;
-        const expectation = input;
+        const input: RepMax = { weight: 100, reps: 1 };
+        const expectation = input.weight;
 
-        const actual = service.calculateOneRepMax(input, 1);
+        const actual = service.calculateOneRepMax(input);
         expect(actual).toBe(expectation);
+    });
+
+    it('should return the same weight for n-rep as the input for n rep', () => {
+        const input = { weight: 148, reps: 7 };
+        const actual = service.calculateNRepMax(input);
+        expect(actual.find(rm => rm.reps === input.reps).weight).toBe(input.weight);
     });
 });
