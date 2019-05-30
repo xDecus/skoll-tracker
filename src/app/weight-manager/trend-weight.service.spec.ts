4ad850fb158f636a11345a9ada5aa7d4d52f003f
Alexander May <guitarx3@gmail.com>
2019-05-30 13:50:27 +0200

    WIP: weight manager
    + trying to get trend weight to work

--- src/app/weight-manager/trend-weight.service.spec.ts
+++ src/app/weight-manager/trend-weight.service.spec.ts
@@ -1,22 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import * as faker from 'faker';
 import { TrendWeightService } from './trend-weight.service';
 import { WeightEntry } from '../models/weight-entry';
+import { Title } from '@angular/platform-browser';
 
-describe('TrendWeightService', () => {
+fdescribe('TrendWeightService', () => {
     let service: TrendWeightService;
-    const ref: WeightEntry = {
-        weight: faker.random.number({ min: 80, max: 90 }),
-        date: new Date(2018, 0, 31).toISOString(),
-        trendWeight: 0,
-        unit: 'metric'
-    };
+    let ref: WeightEntry;
     let entries: WeightEntry[];
 
     beforeEach(() => {
         TestBed.configureTestingModule({});
         service = TestBed.get(TrendWeightService);
         entries = [];
+        ref = {
+            weight: 500,
+            date: new Date(2018, 1, 25).toISOString(),
+            trendWeight: 0,
+            unit: 'metric'
+        };
     });
 
     it('should be created', () => {
@@ -26,10 +27,9 @@ describe('TrendWeightService', () => {
     it('should calculate trend weight for a new entry of today', () => {
         getFakeData(20, entries);
         entries.push(ref);
-        entries.sort((a, b) => service.sortByDate(a.date, b.date));
 
-        const workspace = entries.slice(entries.length - 10);
-        const avgWeight = workspace.map(a => a.weight).reduce((a, b) => a + b) / workspace.length;
+        const workspace = entries.slice(0, service.daysUsedForCalculation);
+        const avgWeight = avg(workspace);
         service.handleTrend(ref, entries);
 
         expect(ref.trendWeight).toBe(avgWeight);
@@ -38,24 +38,50 @@ describe('TrendWeightService', () => {
     it('should correctly handle trend weight for small amounts of entries', () => {
         getFakeData(3, entries);
         entries.push(ref);
-        entries.sort((a, b) => service.sortByDate(a.date, b.date));
 
-        const avgWeight = entries.map(a => a.weight).reduce((a, b) => a + b) / entries.length;
+        const avgWeight = avg(entries);
         service.handleTrend(ref, entries);
 
         expect(ref.trendWeight).toBe(avgWeight);
     });
 
-    it('should handle inserting values in the middle', () => {});
+    fit('should handle inserting values in the middle', () => {
+        getFakeData(20, entries, 2);
+        ref.date = new Date(2018, 0, 12).toISOString();
+        entries.push(ref);
+
+        service.handleTrend(ref, entries);
+        console.log(entries);
+    });
+
+    it('should return future entries', () => {
+        getFakeData(18, entries);
+        console.log(entries);
+        const result = (service as any).getRelevantEntries(9, entries, 10, 'future');
+        expect(result[0].date).toBe(entries[9].date);
+        expect(result[9].date).toBe(entries[0].date);
+    });
+
+    it('should return past entries', () => {
+        getFakeData(19, entries);
+        console.log(entries);
+        const result = (service as any).getRelevantEntries(9, entries, 10, 'past');
+        expect(result[0].date).toBe(entries[9].date);
+        expect(result[9].date).toBe(entries[18].date);
+    });
 });
 
 function getFakeData(amount: number, array: WeightEntry[], step = 1) {
-    for (let i = 0; i < amount; i += step) {
+    for (let i = 0; i < amount * step; i += step) {
         array.push({
-            weight: faker.random.number({ min: 80, max: 90 }),
-            date: new Date(2018, 0, i + 1).toISOString(),
+            weight: i,
+            date: new Date(2018, 0, i + 2).toISOString(),
             trendWeight: 0,
             unit: 'metric'
         });
     }
+}
+
+function avg(arr: WeightEntry[]) {
+    return arr.map(entry => entry.weight).reduce((total, current) => total + current) / arr.length;
 }
--- src/app/weight-manager/trend-weight.service.ts
+++ src/app/weight-manager/trend-weight.service.ts
@@ -5,31 +5,113 @@ import { WeightEntry } from '../models/weight-entry';
     providedIn: 'root'
 })
 export class TrendWeightService {
-    private daysUsedForCalculation = 10;
+    public daysUsedForCalculation = 10;
 
     constructor() {}
 
     handleTrend(reference: WeightEntry, items: WeightEntry[]) {
-        const ix = items.indexOf(reference);
-        // First, let's handle the case where this is actually the newest entry
-        // In that case, the trendWeight of this entry
-        if (ix === items.length - 1) {
-            // Starting at the newest entry, iterate through the last 10 or as many as are in the array
-
-            const weights: number[] = [];
-            for (
-                let i = items.length - 1;
-                i >= 0 && i > items.length - 1 - this.daysUsedForCalculation;
-                i--
-            ) {
-                weights.push(items[i].weight);
-            }
-            reference.trendWeight =
-                weights.reduce((total, current) => total + current) / weights.length;
+        console.log(items);
+        const referenceIndex = items.indexOf(reference);
+        // First, let's handle the case where this is the oldest entry
+        // in since there's no ancestors, its trendWeight is its weight
+        if (referenceIndex === items.length - 1) {
+            reference.trendWeight = reference.weight;
+        }
+        // Then, let's handle the newest entry
+        if (referenceIndex === 0) {
+            console.log(`---- Calc actual weight for ${reference}`);
+            const entries = this.getRelevantEntries(
+                referenceIndex,
+                items,
+                this.daysUsedForCalculation
+            );
+            reference.trendWeight = this.calculateAverage(entries);
+        } else {
+            // Else, the reference is somewhere else in the array.
+            // In that case we'll get us an array that contains the reference
+            // and its trend dependants (the entries that use it for their trend calculation)
+            const entries = this.getRelevantEntries(
+                referenceIndex,
+                items,
+                this.daysUsedForCalculation,
+                'future'
+            );
+            // Then we recalculate each entry's trend weight by passing it into this func
+            entries.forEach(entry => {
+                const relevant = this.getRelevantEntries(
+                    entries.indexOf(entry),
+                    entries,
+                    this.daysUsedForCalculation
+                );
+                this.handleTrend(relevant[0], relevant);
+            });
         }
     }
 
-    sortByDate(x: string, y: string) {
-        return new Date(x).getTime() - new Date(y).getTime();
+    /**
+     * Recalculates the trend weights of the entire given array
+     */
+    public recalculateTrends(items: WeightEntry[]) {
+        for (let i = items.length - 1; i >= 0; i--) {
+            this.handleTrend(items[i], items);
+        }
+    }
+
+    /**
+     * Can be used to sort an array by date, based on iso strings.
+     * * Sorts descending, with the newest date coming first
+     * @param x An ISO date string
+     * @param y An ISO date string
+     */
+    public sortByDate(x: string, y: string): number {
+        return new Date(y).getTime() - new Date(x).getTime();
+    }
+
+    private getRelevantEntries(
+        referenceIndex: number,
+        all: WeightEntry[],
+        daysUsedForCalculation: number,
+        direction: 'past' | 'future' = 'past'
+    ): WeightEntry[] {
+        const entries: WeightEntry[] = [];
+        switch (direction) {
+            case 'future':
+                {
+                    console.log(`---- Future entries for  ${all[referenceIndex].date}----`);
+                    for (
+                        let i = referenceIndex;
+                        i >= 0 && i > referenceIndex - daysUsedForCalculation;
+                        i--
+                    ) {
+                        entries.push(all[i]);
+                    }
+                    console.log(entries);
+                    console.log('---------------------------------------------------------');
+                }
+                break;
+            case 'past':
+                {
+                    console.log(`---- Past entries for  ${all[referenceIndex].date}----`);
+                    for (
+                        let i = referenceIndex;
+                        i < all.length && i < referenceIndex + daysUsedForCalculation;
+                        i++
+                    ) {
+                        entries.push(all[i]);
+                    }
+                    console.log(entries);
+                    console.log('---------------------------------------------------------');
+                }
+                break;
+        }
+
+        return entries;
+    }
+
+    private calculateAverage(entries: WeightEntry[]): number {
+        return (
+            entries.map(entry => entry.weight).reduce((total, current) => total + current) /
+            entries.length
+        );
     }
 }
